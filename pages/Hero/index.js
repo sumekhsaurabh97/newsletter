6fc14fc9eb7009f44df48b8a4790d6a3447f1639
sumekh saurabh <sumekhsaurabh97@gmail.com>
2023-07-21 00:41:14 +0530

    feat: catogery feature added

--- pages/Hero/index.js
+++ pages/Hero/index.js
@@ -1,44 +1,70 @@
 import React from "react";
 import Link from "next/link";
+import { useRef } from "react";
+import Router from "next/router";
 
 import styles from "./styles/hero.module.scss";
 import Header from "../../components/header";
 
 export default function Hero() {
+  const btnCatogery = useRef();
+  console.log(btnCatogery.current);
+  let handleClick = (event) => {
+    let btnText = event.target.innerText;
+    let catogery = btnText.split(" ");
+    let catogeryName = catogery[catogery.length - 1];
+
+    Router.push({
+      pathname: "/email-verification",
+      query: {
+        catogeryName,
+      },
+    });
+  };
+
+  let technologies = ["SASS", "Bootstrap"];
   return (
     <div>
       <Header />
-     <div className={styles.heroSection_outer}>
-     <div className={`mt-5 text-center container ${styles.Hero_section}`}>
-        <h1 className="px-2">
-          <span>ナ</span>hink things through
-        </h1>
-        <h3 className={`px-2 mt-3 ${styles.title}`}>
-          A daily long-form essay to make you smarter about technology,
-          productivity, and AI. 
-          Trusted by 80K+ builders.
-        </h3>
-
-        <Link href="./email-verification">
-          <button>Start a Trial</button>
-        </Link>
-        {/* <button>Start a Trial</button> */}
-        <div className={styles.underline_vedio_container}>
-          <video
-            id="underline-video"
-            width="100%"
-            autoplay="true"
-            defaultmuted=""
-            playsinline=""
-          >
-            <source
-              src="https://d24ovhgu8s7341.cloudfront.net/static/underline.mp4"
-              type="video/mp4"
-            ></source>
-          </video>
+      <div className={styles.heroSection_outer}>
+        <div className={`mt-5 text-center container ${styles.Hero_section}`}>
+          <h1 className="px-2">
+            <span>ナ</span>hink things through
+          </h1>
+          <h3 className={`px-2 mt-3 ${styles.title}`}>
+            A daily long-form essay to make you smarter about technology,
+            productivity, and AI. Trusted by 80K+ builders.
+          </h3>
+          {technologies.map((i) => {
+            return (
+              <Link
+                href="./email-verification"
+                className="d-block mt-3"
+                id="btn"
+              >
+                <button ref={btnCatogery} onClick={handleClick}>
+                  Start a Trial with {i}
+                </button>
+              </Link>
+            );
+          })}
+          {/* <button>Start a Trial</button> */}
+          <div className={styles.underline_vedio_container}>
+            <video
+              id="underline-video"
+              width="100%"
+              autoplay="true"
+              defaultmuted=""
+              playsinline=""
+            >
+              <source
+                src="https://d24ovhgu8s7341.cloudfront.net/static/underline.mp4"
+                type="video/mp4"
+              ></source>
+            </video>
+          </div>
         </div>
       </div>
-     </div>
     </div>
   );
 }
--- pages/email-verification/index.js
+++ pages/email-verification/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Router, { useRouter } from "next/router";
 import Link from "next/link";
 import { Form, Input, Button } from "antd";
 
@@ -11,6 +12,14 @@ export default function EmailVerification() {
   const [userEmail, setUserEmail] = useState("");
   const [emailInputCard, setEmailInputCard] = useState(true);
   const [form] = Form.useForm();
+
+  const router = useRouter();
+  const {
+    query: { catogeryName },
+  } = router;
+  const props = { catogeryName };
+  console.log(props);
+
   const handleChange = (event) => {
     setUserEmail(event.target.value);
   };
@@ -27,14 +36,13 @@ export default function EmailVerification() {
       Headers: { "content-type": "application/json" },
       body: JSON.stringify({
         email: userEmail,
-        category: "string",
+        category: props,
         user_referral_code: "string",
       }),
     };
     await fetch("https://jsonplaceholder.typicode.com/posts", option)
       .then((response) => response.json())
       .then((json) => console.log(json));
-    console.log(userEmail);
   };
 
   return (
@@ -94,7 +102,7 @@ export default function EmailVerification() {
           </p>
         </div>
       ) : (
-        <PopUp/>
+        <PopUp />
       )}
     </div>
   );
